7cd3f62701f87b4d60de9fc72a9642b8dbaee0b0
Rishabh Jain <rishabhjain@Rishabhs-MacBook-Air.local>
2025-07-31 00:46:29 +0530

    render

--- server.js
+++ server.js
@@ -251,10 +251,10 @@ dataRouter.delete('/budgets/:id', async (req, res) => {
 app.use('/api', dataRouter);
 
 // --- Serve Frontend ---
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 // 8. Start Server
 app.listen(PORT, () => {
